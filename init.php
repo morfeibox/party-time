9c97a628b9337d5052d906d729c68ac3b8659d59
Atanas Beychev <abeychev@dd-fs.co.uk>
2018-08-10 14:56:57 +0300

    small cose changes

--- init.php
+++ init.php
@@ -50,12 +50,11 @@ function init(){
     function setModel($m)    {
         $model = parent::setModel($m);
       
-        
-       $count = $model->action('count')->getOne();
+      
        $avarageAge= $model->action('fx',['avg', 'age'])->getOne();
-      // $avarageAge = ($ageSum / $count);
+    
        $this->template['avarageAge'] = $avarageAge;
-       $this->template['guests'] = $count;
+       $this->template['guests'] = $model->action('count')->getOne();
        $this->template['drinks'] = $model->action('fx',['sum', 'units_of_drink'])->getOne();
        $this->template['women'] = $model->addCondition('gender', 'female')->action('count')->getOne();
        $this->template['men'] = $model->addCondition('gender', 'male')->action('count')->getOne();
